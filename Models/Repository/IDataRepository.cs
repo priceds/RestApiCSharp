e23e4ac00349faf4ef76b077b42b8d4b68ecc991
Sarvesh <amsarvesh@live.com>
2021-12-28 13:00:40 +0530

    Rest Api First Commit

---
+++ Models/Repository/IDataRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using RestFull.Models;
+namespace RestFull.Models.Repository
+{
+    public interface IDataRepository<Employee>
+    {
+        IEnumerable<Employee> GetAll();
+        Employee Get(long id);
+        void Add(Employee entity);
+        void Update(Employee dbEntity, Employee entity);
+        void Delete(Employee entity);
+
+
+    }
+}
